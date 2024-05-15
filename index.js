0d4e44cefb685caf3fbd92c8ed3a4cf96e7d2abb
j-sivertson <joshsivertson@outlook.com>
2024-05-14 23:19:33 -0600

    Created and tested my /shoes endpoint

--- index.js
+++ index.js
@@ -7,44 +7,36 @@ const redisClient = redis.createClient({
     host: 'localhost',
     port: 6379
 }); //Creating a redis client
-
 redisClient.on('error', (err) => console.log('Redis Client Error:', err)); //If there is an error, log it from redis
+redisClient.on('connect', () => console.log('DB connected')); //If the connection is successful, log it
+redisClient.connect();
 
-app.use(express.json()); //Making all the requests JSON
+app.use(express.json()); //Making all payloads be returned to the express server as json
+//Basic Hello world for the home page
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+shoe = {
+    id: 1,
+    name: 'Nike',
+    price: 101
+}
 
 //defining what to do when there is a post request on /shoes
-app.post('/shoes', (req, res) => {
-    const { id, name, price } = req.body;
-    const shoeObject = { id, name, price };
-
-    redisClient.set(id, JSON.stringify(shoeObject), (err, reply) => {
-        if (err) {
-            console.error('Error storing shoe:', err);
-            res.status(500).json({ error: 'Failed to store shoe in database' });
-        } else {
-            console.log('Shoe stored successfully');
-            res.status(201).json({ message: 'Shoe stored successfully' });
-        }
-    });
+app.post('/shoes', async (req, res) => {
+    await redisClient.set('shoe:1', JSON.stringify(shoe));
+    res.send('Shoe added');
+    console.log('Shoe added');
 });
 
 //defining what to do when there is a get request on /shoes
-app.get('/shoes/:id', (req, res) => {
-    const shoeId = req.params.id;
-
-    redisClient.get(shoeId, (err, reply) => {
-        if (err) {
-            console.error('Error retrieving shoe:', err);
-            res.status(500).json({ error: 'Failed to retrieve shoe from database' });
-        } else if (reply) {
-            const shoeObject = JSON.parse(reply);
-            res.status(200).json(shoeObject);
-        } else {
-            res.status(404).json({ error: 'Shoe not found' });
-        }
-    });
+app.get('/shoes', async (req, res) => {
+    console.log('Received GET request to /shoes');
+    const getShoe = await redisClient.get('shoe:1');
+    res.send(getShoe);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
