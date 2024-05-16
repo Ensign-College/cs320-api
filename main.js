2b47b6704336323a712fbb81e7356d001d7b96cc
Brayden Connole <brayden.connole@gmail.com>
2024-05-15 22:02:38 -0600

    shoes and shoes:id

--- main.js
+++ main.js
@@ -16,27 +16,40 @@ var corsOptions = {
 }
 
 app.use(cors(corsOptions))
+app.use(express.json())
 
-app.get('/', function (req, res) {
+
+app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.post('/shoes', function (req, res) {
+app.post('/shoes', async (req, res) => {
   console.log('POST /shoes')
-  const shoe = {
-      'id': 1234,
-      'name': 'My sick red adidas',
-      'color': 'red',
-    }
-  redisClient.set('shoe', JSON.stringify(shoe))
-  res.send(shoe)
+
+  let shoe = req.body
+  try {
+    shoe.id = parseInt(await redisClient.json.arrLen('shoes','$'))+1
+  } catch(e) {
+    shoe.id = 1
+  }
+  console.log(shoe)
+
+  await redisClient.json.arrAppend('shoes','$', shoe)
+  res.json(shoe)
 })
 
-app.get('/shoes', async function(req, res) {
+app.get('/shoes', async (req, res) => {
   console.log('GET /shoes')
-  let shoes = await redisClient.get('shoe')
+  let shoes = await redisClient.json.get('shoes',{path:'$'})
   console.log(shoes)
-  res.json(shoes)
+  res.json(shoes[0])
+})
+
+app.get('/shoes/:id', async (req, res) => {
+  let shoeId = Number(req.params.id)
+  let shoes = await redisClient.json.get('shoes', {path: `$`})
+  let shoe = shoes[0].find(({id}) => id === shoeId)
+  res.send(shoe)
 })
 
 app.listen(3001)
