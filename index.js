a90417945b82b27969c57a3c443a0d3ee0e7b955
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2024-05-18 00:53:45 -0600

    Api can create more than just 2 pair of shoes and can filter

--- index.js
+++ index.js
@@ -1,56 +1,76 @@
 // Load modules
 // Express
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Redis
-const redis = require('redis');
+const redis = require("redis");
 
 // CORS
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Create Redis Client
 const redisClient = redis.createClient({
-    host: 'localhost',
-    port: 6379
+  host: "localhost",
+  port: 6379,
 });
 
 // Connect to Redis
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
 redisClient.connect();
 
-app.get('/', async (req, res) => {
-    res.send('Hello World!');
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/shoes', async (req, res) => {
-    if(req.body.color) {
-        const shoesGet = await redisClient.json.get('shoe:', 'color', req.body.color);
-        return res.send(shoesGet);
-    } else {
-        const shoesGet = await redisClient.json.get('shoe:');
-        return res.send(shoesGet);
-    }
-});
-
-app.post('/shoes', async (req, res) => {
-    const shoe = {
-        id: 1,
-        name: 'Nike Air Max 90',
-        color: 'White',
-        price: 100
-    }
-    await redisClient.json.set('shoe:', '$', shoe, (err, reply) => {
-        if (err) {
-            console.log(err);
+app.get("/shoes", async (req, res) => {
+  const query = req.query;
+  if (query.owner) {
+    const owner = query.owner;
+    try {
+      const shoe = await redisClient.lRange(owner, 0, -1);
+      if (shoe.length === 0) {
+        const owners = await redisClient.keys("*");
+        if (owners.includes(owner)) {
+          return res.send([{ owner: owner, shoes: [] }]);
         }
-    });
-    return res.send('Shoe added')
+        throw new Error("Owner not found");
+      }
+      const listshoe = shoe.map((shoe) => JSON.parse(shoe));
+      const ownerShoes = { owner: owner, shoes: listshoe };
+      return res.send(ownerShoes);
+    } catch (error) {
+      return res.status(404).send(error.message);
+    }
+  }
+  const owners = await redisClient.keys("*");
+  const shoes = [];
+  for (const owner of owners) {
+    const shoe = await redisClient.lRange(owner, 0, -1);
+    const listshoe = shoe.map((shoe) => JSON.parse(shoe));
+    const ownerShoes = { owner: owner, shoes: listshoe };
+    shoes.push(ownerShoes);
+  }
+  res.send(shoes);
+});
+
+app.post("/shoes", async (req, res) => {
+  const owners = ["John", "Jane", "Doe"];
+  const colors = ["red", "blue", "green"];
+  const brands = ["nike", "adidas", "reebok"];
+  const shoe = {
+    id: Math.floor(Math.random() * 1000),
+    brand: brands[Math.floor(Math.random() * brands.length)],
+    color: colors[Math.floor(Math.random() * colors.length)],
+  };
+  const owner = owners[Math.floor(Math.random() * owners.length)];
+  await redisClient.rPush(owner, JSON.stringify(shoe));
+  return res.json("Shoe added to Redis");
 });
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
