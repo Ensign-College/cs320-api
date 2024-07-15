fb5e5a60c77c4956a7fce25e990a52b834073232
erickwduranr <161395542+erickwduranr@users.noreply.github.com>
2024-07-15 14:16:44 -0600

    lastversion

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+console.log('Creating express app')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('GET /')
+    res.send('Hello World')
+})
+
+app.listen(3001)
